929215204d121e5046927e16bd15b4482f48ccec
Cody Moore <codymoore240@gmail.com>
2019-03-01 13:44:43 -0500

    Generates a token

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const app = require('express')()
 const http = require('http').createServer(app)
 var io = require('socket.io')(http)
 
+//import utils
+import generateToken from 'generateToken'
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello worlds</h1>')
 })
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
+  //create a token
+  const token = generateToken()
+
+  console.log(`a user connected with a token of ${token}`)
+
+  //on disconnect event
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 })
 
-http.listen(3000, () => {
-  console.log('listening on *:3000')
+http.listen(3001, () => {
+  console.log('listening on *:3001')
 })
