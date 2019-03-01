5de3cd6fca0ecaa5f3ff8804f9f94fae61be0347
Cody Moore <codymoore240@gmail.com>
2019-03-01 14:55:11 -0500

    Added generic move event

--- index.js
+++ index.js
@@ -5,17 +5,60 @@ var io = require('socket.io')(http)
 //import modules
 const generateToken = require('./generateToken')
 
+//board game data
+const gameData = {
+  board: [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0]
+  ],
+  player1: null,
+  player2: null,
+  currentTurn: 0
+}
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello worlds</h1>')
 })
 
 io.on('connection', (socket) => {
+  let yourToken = null
   //create a token
   const token = generateToken.generate()
   console.log(`a user connected with a token of ${token}`)
-  //send the token
-  socket.send(token)
 
+  //Assign token logic
+  if (gameData.player1 === null) {
+    //set the current user as player1
+    gameData.player1 = token
+    yourToken = token
+  } else if (gameData.player2 === null){
+    //set the current user as player2
+    gameData.player2 = token
+    yourToken = token
+  } else {
+    //This is just a spectator
+  }
+
+  const payload = {
+    ...gameData,
+    yourToken
+  }
+
+  //send the token
+  socket.emit('newToken', payload)
+
+  //Move Event
+  socket.on('move', (id, x, y) => {
+    console.log(`A move event was detected`)
+
+    //modify the data
+    socket.emit('updateBoard', gameData)
+  })
 
   //on disconnect event
   socket.on('disconnect', () => {
